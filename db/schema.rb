7cdce99b0aff3c55e8f7cd0ebdd6c279ca4614cd
Daniel Ramiro Lanz <danielramiro91@hotmail.com>
2022-08-29 17:55:07 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_151748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -33,6 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_151748) do
     t.integer "people_limit"
     t.string "status"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -61,4 +61,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_151748) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
