5ddc0cfa3f993fc403c1e3729865ba1447ab72c2
Maria Cendoya <m.j.cendoya92@gmail.com>
2022-08-30 10:56:20 +0200

    Activity categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_154842) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_085219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_154842) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "activity_categories", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_activity_categories_on_activity_id"
+    t.index ["category_id"], name: "index_activity_categories_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -73,5 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_154842) do
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "activity_categories", "activities"
+  add_foreign_key "activity_categories", "categories"
   add_foreign_key "dogs", "users"
 end
