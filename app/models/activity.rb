f682fa29f8e34e04ef6c93f8b7d64ff5c85ffdce
Felipe Fischel <felipefischel@gmail.com>
2022-08-30 17:50:10 +0200

    started working on the activity views

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,12 +7,12 @@ class Activity < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
-  validates :description, presence: true, lenght: { minimum: 15 }
+  validates :description, presence: true, length: { minimum: 15 }
   validates :location, presence: true
   validates :start_date, :end_date, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true, comparison: { greater_than: :start_time }
-  validates :category # inclusion to be created
+  # validates :category # inclusion to be created
   validates :dog_limit, presence: true, numericality: { only_integer: true }
   validates :people_limit, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true }
