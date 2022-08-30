8e3806cfa1d8169d039eaca8de86cef1059adf5a
Daniel Ramiro Lanz <danielramiro91@hotmail.com>
2022-08-30 10:44:35 +0200

    comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema[7.0].define(version: 2022_08_29_154842) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,14 +29,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_154842) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
-
   end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
