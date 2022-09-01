0b2e967ec6badfe83253d0f954e90cc39b513f50
Maria Cendoya <m.j.cendoya92@gmail.com>
2022-09-01 12:31:28 +0200

    geocode initial installation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
   has_many :activity_categories, dependent: :destroy
   has_many :categories, through: :activity_categories
