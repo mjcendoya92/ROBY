f746cf077aba5fd27569e1589333a898feb140af
Daniel Ramiro Lanz <danielramiro91@hotmail.com>
2022-09-02 15:44:30 +0200

    added more features to chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+  
+    # @chatroom = Chatroom.find_by(params[:chatroom_id])
     @message = Message.new
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,10 @@ class PagesController < ApplicationController
         image_url: helpers.asset_url("https://res.cloudinary.com/mariacend1910/image/upload/v1661948505/images_5_ugq1bx.png")
       }
     end
+    # @chatroom = Chatroom.find_by(params[:chatroom_id])
   end
 
   def profile
     @dogs = current_user.dogs
   end
-
 end
