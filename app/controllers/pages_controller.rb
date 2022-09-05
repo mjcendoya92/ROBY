887c23b132160bba9a7827eb263052374d06be2b
Maria Cendoya <m.j.cendoya92@gmail.com>
2022-09-05 11:34:36 +0200

    fixed map pop up messages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
       {
         lat: activity.latitude,
         lng: activity.longitude,
+        info_window: render_to_string(partial: "activities/info_window", locals: { activity: activity }),
         image_url: helpers.asset_url("https://res.cloudinary.com/mariacend1910/image/upload/v1661948505/images_5_ugq1bx.png")
       }
     end
