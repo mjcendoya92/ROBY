4d11e627a2114afaf0881a3458675441582f24d3
Maria Cendoya <m.j.cendoya92@gmail.com>
2022-09-06 23:04:18 +0200

    fixed category cards filter

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -14,6 +14,7 @@ class ActivitiesController < ApplicationController
       @activities = @activities.joins(:activity_categories).where(activity_categories: { category_id: params[:category]})
     end
 
+
     @markers = @activities.geocoded.map do |activity|
       {
         lat: activity.latitude,
